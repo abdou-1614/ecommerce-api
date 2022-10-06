a3ca6746f37ebdc920a63d2f5ad421274f05fc98
Abdou <kirakira1614@gmail.com>
2022-10-06 23:01:15 +0200

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
 })
 export class AppModule {}
---
+++ src/utils/tokenExpirationDate.ts
@@ -0,0 +1,19 @@
+import { refreshJwtConfig } from './../config/jwt.config';
+import ms from 'ms'
+
+export function TokenExpirationDate(): Date {
+    const expireIn = ms(String(refreshJwtConfig.expiresIn)) / 1000 / 60 / 60 / 24
+    const expireAt = AddDays(expireIn)
+    return expireAt
+}
+
+
+/**
+ * Add Amount Of Days Start From Today
+ */
+
+function AddDays(days: number): Date {
+    const result = new Date()
+    result.setDate(result.getDate() + days)
+    return result
+}
