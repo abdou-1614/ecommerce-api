98e8595a04ea5057a10e71e0a262043600973856
Abdou <kirakira1614@gmail.com>
2022-10-07 00:59:21 +0200

    structure folders

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
+import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService]
 })
