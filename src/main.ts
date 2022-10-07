38e261f5442a4f510100f120538caedbceef28b7
Abdou <kirakira1614@gmail.com>
2022-10-08 00:31:38 +0200

    Configuration Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+  .setTitle('Ecommerce BackEnd')
+  .setDescription('BackEnd Ecommerce App')
+  .setVersion('0.0.1')
+  .addBearerAuth()
+  .addTag('authentication')
+  .addTag('user')
+  .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'E-commerce Swagger API'
+  })
   await app.listen(3000);
 }
 bootstrap();
