806f919f2934811f772ff4a85206de4acbe28022
Abdou <kirakira1614@gmail.com>
2022-10-09 22:46:51 +0200

    Add Rresh Token

--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -4,11 +4,11 @@ export class LoginResponseDto {
    * Access JWT Authentication token
    * @example "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5NWFkMzNhNS0wYjk465fdgdfrtYjFmMS1hMTkwNzM0NWJmYzciLCJpYXQiOjE2NDg0NzU3MzEsImV4cCI6MTY0ODQ3NjYzMX0.h3z3JDvHOi6y5C_N0Kt6tdP2nWK_dHBZxioQn7VANNo"
    */
-    accesstoken: string
+    accessToken: string
 
       /**
    * Refresh JWT Authentication token
    * @example "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5NWFkMzNhNS0wYjk4LTQ2ODYtYjFmMS1hMTkwNzGT76JmYzciLCJpYXQiOjE2NDg0NzU3MzEsImV4cCI6MTY0ODQ3NjYzMX0.h3z3JDvHOi6y5C_N0Kt6tdP2nWK_dHBZxioQn7VANNo"
    */
-    refreshtoken: string
+    refreshToken: string
 }
