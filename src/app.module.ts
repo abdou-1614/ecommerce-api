c85db00d46f9c7edb6871f818ce759f490b8478f
Abdou <kirakira1614@gmail.com>
2022-10-09 22:52:13 +0200

    add AccessJwtGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
+import { AccessJwtGuard } from './auth/auth-access-jwt.guard';
 import { Module } from '@nestjs/common';
+import {APP_GUARD} from '@nestjs/core'
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AccessJwtGuard
+    }
+  ]
 })
 export class AppModule {}
