d263cdeaf5b71a4900c49da377f2bdbe91418f43
Abdou <kirakira1614@gmail.com>
2022-10-13 22:31:48 +0200

    Implement Role Guard For Admin

--- src/common/guards/role.guard.ts
+++ src/common/guards/role.guard.ts
@@ -1,14 +1,15 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import {Reflector} from '@nestjs/core'
 import { Role } from "@prisma/client";
 import { Observable } from "rxjs";
 import { IS_ADMIN } from "../decorators/isAdmin.decorator";
 
+@Injectable()
 export class RoleGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const isAdmin = this.reflector.getAllAndOverride(IS_ADMIN, [
+        const isAdmin = this.reflector.getAllAndOverride<boolean>(IS_ADMIN, [
             context.getHandler(),
             context.getClass()
         ])
