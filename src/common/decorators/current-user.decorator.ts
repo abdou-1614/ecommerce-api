073de224c59a856b1f8d42b0abb9717b798bb742
Abdou <kirakira1614@gmail.com>
2022-10-13 22:24:47 +0200

    Updated CurrentUser Decorator

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,8 +1,7 @@
 import {createParamDecorator, ExecutionContext} from '@nestjs/common'
 export const CurrentUser = createParamDecorator(
-    (data: string ,context: ExecutionContext) => {
+    (data: unknown ,context: ExecutionContext) => {
         const request = context.switchToHttp().getRequest()
-        const user = request.user
-        return data ? user?.[data] : user
+        return request.user['userId']
     }
 )
