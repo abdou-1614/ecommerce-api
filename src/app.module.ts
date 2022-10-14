5f433c188c1b0f65309f30839d4f312c4205fb1e
Abdou <kirakira1614@gmail.com>
2022-10-14 23:22:54 +0200

    Add Products Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import {APP_GUARD} from '@nestjs/core'
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, ProductsModule],
   providers: [
     {
       provide: APP_GUARD,
