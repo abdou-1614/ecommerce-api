21a94ae34c41e98b25cabe1d65e6bd0198f0ff89
Abdou <kirakira1614@gmail.com>
2022-10-15 23:17:28 +0200

    Implement Create Product For Controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { isAdmin } from 'src/common/decorators/isAdmin.decorator';
+import { CreateProductsDto } from './dto/create-product.dto';
+import { Products } from './entities/products.entity';
+import { FileToBodyInterceptor } from './interceptors/file.interceptor';
 import { ProductsService } from './products.service';
 
+@ApiTags('Product')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @ApiOperation({summary: 'Admin Create Product'})
+  @ApiOkResponse({
+    type: Products
+  })
+  @ApiBody({ type: CreateProductsDto })
+  @ApiConsumes('multipart/form-data')
+  @isAdmin()
+  @Post()
+  @UseInterceptors(FileInterceptor('image'), FileToBodyInterceptor)
+  async createProduct(@Body() CreateProduct: CreateProductsDto): Promise<Products> {
+    return this.productsService.CreateProduct(CreateProduct)
+  }
 }
