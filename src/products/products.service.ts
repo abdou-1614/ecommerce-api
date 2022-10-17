4cddca84d10f25a4399a13c06b8641e7624947ae
Abdou <kirakira1614@gmail.com>
2022-10-17 22:08:27 +0200

    implement Create Product Service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,57 @@
+import { CreateProductsDto } from './dto/create-product.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { Products } from './entities/products.entity';
+import { File } from './types/file';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(private prisma: PrismaService) {}
+
+    async CreateProduct(createProduct: CreateProductsDto): Promise<Products> {
+        const urlName = this.formatUrlName(createProduct.name)
+        const categories = this.connectCategoryByID(createProduct.categories)
+        const product = await this.prisma.product.create({
+            data: {
+                ...createProduct,
+                image: createProduct.image.filename,
+                urlName,
+                categories
+            }
+        })
+
+        return product
+    }
+
+   /** Formats the name to generate an urlName.
+   *
+   * Makes the name lower case, remove leading and trailing white spaces,
+   * turn to single the multiple spaces between words and make
+   * single spaces hyphens
+   *
+   * @example " SmArT    wAtCh   " becomes "smart-watch"
+   */
+    private formatUrlName(name: string): string {
+
+        const lowerCaseUrlName = name.toLocaleLowerCase()
+        const trimUrlName = lowerCaseUrlName.trim()
+        const singleSpaceUrlName = trimUrlName.replace(/\s\s+/g, ' ')
+        const spaceToHyphenUrlName  = singleSpaceUrlName.split(' ').join('-')
+        return spaceToHyphenUrlName
+    }
+    /**
+   * Format the categories IDs array into the prisma query way
+   */
+
+    private connectCategoryByID(categories: string[]): Prisma.CategoryUncheckedCreateNestedManyWithoutProductsInput {
+        let categoriesConnection = { connect: [] }
+
+        if(categories) {
+            categoriesConnection = {
+                connect: categories.map(category => {id: category}) 
+            }
+        }
+        return categoriesConnection
+    }
+}
