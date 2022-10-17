5afb89dcac72a2f2e57d44ec4781ff9c799bc71d
Abdou <kirakira1614@gmail.com>
2022-10-17 22:10:24 +0200

    Enable class transformer pipe

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }))
 
   const config = new DocumentBuilder()
   .setTitle('Ecommerce BackEnd')
