b3a0d89a7e630c5393f142499ffecd3b5a7d75b0
Abdou <kirakira1614@gmail.com>
2022-10-18 23:30:30 +0200

    Get All Products With Pagination Of 10 Products In 1 Page

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,14 @@
+import { FindProductDto } from './dto/find-product.dto';
 import { ProductResponse } from './dto/product-response.dto';
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { isAdmin } from 'src/common/decorators/isAdmin.decorator';
 import { CreateProductsDto } from './dto/create-product.dto';
 import { Products } from './entities/products.entity';
 import { FileToBodyInterceptor } from './interceptors/file.interceptor';
 import { ProductsService } from './products.service';
+import { Public } from 'src/auth/public.decorator';
 
 @ApiTags('Product')
 @Controller('products')
@@ -24,5 +26,16 @@ export class ProductsController {
   @UseInterceptors(FileInterceptor('image'), FileToBodyInterceptor)
   async createProduct(@Body() CreateProduct: CreateProductsDto): Promise<Products> {
     return this.productsService.CreateProduct(CreateProduct)
+  }
+
+
+  @ApiOkResponse({type: ProductResponse})
+  @ApiOperation({summary: 'Returns All Products', description: `Returns all products with pagination,      
+  Default is starting on page 1 showing 10 results per page,
+  searching and ordering by name`})
+  @Public()
+  @Get()
+  async getProducts(@Query() findAllProductDto: FindProductDto): Promise<Products[]> {
+    return this.productsService.findAllProduct(findAllProductDto)
   }
 }
