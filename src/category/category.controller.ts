3dc14c792ed6e4ecf1c732e37049b0d0129a7a1a
Abdou <kirakira1614@gmail.com>
2022-10-23 23:00:58 +0200

    Add Create Category Handler In Controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,20 @@
+import { Category } from './entities/category.entity';
+import { CreateCategoryDto } from './dto/create-category.dto';
 import { CategoryService } from './category.service';
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { isAdmin } from 'src/common/decorators/isAdmin.decorator';
 
-
+@ApiTags('Category')
 @Controller()
 export class CategoryController {
     constructor(private categoryService: CategoryService) {}
+
+    @ApiOperation({summary: 'Admin Create Category'})
+    @ApiCreatedResponse({type: Category})
+    @isAdmin()
+    @Post()
+    async create(@Body() create: CreateCategoryDto): Promise<Category> {
+        return this.categoryService.createCategory(create)
+    }
 }
