f6e43a8a8339a2f4a96adf0df7d1c9dba751445f
Abdou <kirakira1614@gmail.com>
2022-10-23 23:37:50 +0200

    Implement Find Category Handler

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,9 +1,12 @@
+import { FindProductDto } from './../products/dto/find-product.dto';
 import { Category } from './entities/category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { CategoryService } from './category.service';
-import { Body, Controller, Post } from "@nestjs/common";
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { isAdmin } from 'src/common/decorators/isAdmin.decorator';
+import { FindCategoryDto } from './dto/find-category.dto';
+import { Public } from 'src/auth/public.decorator';
 
 @ApiTags('Category')
 @Controller()
@@ -16,5 +19,21 @@ export class CategoryController {
     @Post()
     async create(@Body() create: CreateCategoryDto): Promise<Category> {
         return this.categoryService.createCategory(create)
+    }
+
+    @ApiOperation({summary: 'Return All Products', description: 'Returns all categories with pagination'})
+    @ApiOkResponse({type: Category})
+    @Public()
+    @Get()
+    async findAllCategory(@Query() findCategory: FindCategoryDto): Promise<Category[]> {
+        return this.categoryService.findAll(findCategory)
+    }
+
+    @ApiOperation({summary: 'Get Categories And Products Have This Category', description: 'Find category by ID, only for admins'})
+    @ApiOkResponse({type: Category})
+    @isAdmin()
+    @Get('/id/:id')
+    async findById(@Param('id') id: string, @Query() findProducts: FindProductDto) {
+        return this.categoryService.findOneById(id, findProducts)
     }
 }
