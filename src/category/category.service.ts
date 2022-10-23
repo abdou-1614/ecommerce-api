cab7a2a78a64e744cdae7d7ae3b7571c22012280
Abdou <kirakira1614@gmail.com>
2022-10-23 23:01:26 +0200

    Create Category Service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,7 +1,26 @@
+import { CreateCategoryDto } from './dto/create-category.dto';
 import { PrismaService } from './../prisma/prisma.service';
 import {Injectable} from '@nestjs/common'
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
     constructor(private prisma: PrismaService) {}
+
+    async createCategory(createCategort: CreateCategoryDto): Promise<Category> {
+        const name = this.capitalizeFirstLatter(createCategort.name)
+        const category = await this.prisma.category.create({
+            data: {
+                ...createCategort,
+                name
+            }
+        })
+        return category
+    }
+
+    /** Capitalize only the first letter of the category name */
+
+    private capitalizeFirstLatter(name: string) {
+        return name[0].toUpperCase + name.substring(1).toLocaleLowerCase()
+    }
 }
