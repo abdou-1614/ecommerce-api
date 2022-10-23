f071af5c8a7cc81c0357d7a8ea7cb0ec6079c471
Abdou <kirakira1614@gmail.com>
2022-10-23 23:00:08 +0200

    Add ApiProperty Swagger Decorator

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Prisma } from '@prisma/client';
 export class Category implements Prisma.CategoryUncheckedCreateInput {
    /**
    * Category ID as UUID
    * @example "e6cf9a58-438c-4fce-8d85-db3d22db270a"
    */
+    @ApiProperty({
+        example: 'e6cf9a58-438c-4fce-8d85-db3d22db270a'
+    })
     id?: string;
 
    /**
    * Category name
    * @example "Decoration"
    */
+    @ApiProperty({
+        example: 'SmartPhones'
+    })
     name: string;
 }
