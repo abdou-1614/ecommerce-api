9a1c981e8b1ff9d83af4b0d77b7b9e5fb1d73cc9
Abdou <kirakira1614@gmail.com>
2022-10-23 23:38:49 +0200

    Find Category By ID And Its Products

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,7 +1,9 @@
+import { FindCategoryDto } from './dto/find-category.dto';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { PrismaService } from './../prisma/prisma.service';
 import {Injectable} from '@nestjs/common'
 import { Category } from './entities/category.entity';
+import { FindProductDto } from 'src/products/dto/find-product.dto';
 
 @Injectable()
 export class CategoryService {
@@ -14,6 +16,33 @@ export class CategoryService {
                 ...createCategort,
                 name
             }
+        })
+        return category
+    }
+
+    async findAll({categoryName = '', offset = 10, page = 0}: FindCategoryDto): Promise<Category[]> {
+        const categoryToSkip = (page - 1) * offset
+        return this.prisma.category.findMany({
+            skip: categoryToSkip,
+            take: offset,
+            where: {name: {contains: categoryName, mode: 'insensitive'}},
+            orderBy: {name: 'asc'}
+        })
+    }
+
+    async findOneById(id: string, {productName = '', page = 0, offset = 10}: FindProductDto): Promise<Category> {
+        const productToSkip = (page - 1) * offset
+        const category = await this.prisma.category.findUnique({
+            where: {id},
+            include: {
+                products: {
+                    select: {id: true, name: true, image: true, urlName: true},
+                    where: {name: {contains: productName, mode: 'insensitive'}},
+                    skip: productToSkip,
+                    take: offset
+                }
+            },
+            rejectOnNotFound: true
         })
         return category
     }
