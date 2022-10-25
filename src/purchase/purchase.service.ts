9bdd2f1c283f97894fa2eeac6c5830a9832b7bf4
Abdou <kirakira1614@gmail.com>
2022-10-26 00:04:33 +0200

    Implement Create Purchase

--- src/purchase/purchase.service.ts
+++ src/purchase/purchase.service.ts
@@ -1,4 +1,40 @@
+import { CreatePurchaseDto } from './dto/create-purchase.dto';
+import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import currency from 'currency.js';
+import { Purchase } from './entities/purchase.entity';
 
 @Injectable()
-export class PurchaseService {}
+export class PurchaseService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(userId: string, creatPurchase: CreatePurchaseDto): Promise<Purchase> {
+        const totalPrice = await this.calculateTotalPrice(creatPurchase)
+
+        const purchase = await this.prisma.purchase.create({
+            data: {
+                ...creatPurchase,
+                totalPrice,
+                userId,
+            },
+            include: {
+                user: {select: {name: true}},
+                product: {select: {name: true}}
+            }
+        })
+        return purchase
+    }
+
+
+    private async calculateTotalPrice({productId, amount}: CreatePurchaseDto): Promise<number> {
+        const {price} = await this.prisma.product.findUnique({
+            where: {
+                id: productId
+            }
+        })
+
+        const totalPrice = currency(price.toNumber()).multiply(amount)
+
+        return totalPrice.value
+    }
+}
