04a33f4cdcc451da91ae78cbda36a36950eed64d
Abdou <kirakira1614@gmail.com>
2022-10-25 15:04:55 +0200

    Init File

---
+++ src/purchase/purchase.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { PurchaseService } from './purchase.service';
+
+@Controller('purchase')
+export class PurchaseController {
+  constructor(private readonly purchaseService: PurchaseService) {}
+}
