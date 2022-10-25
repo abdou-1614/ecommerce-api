1485e523cabca058a0fcd0375be8952816f6fd63
Abdou <kirakira1614@gmail.com>
2022-10-26 00:03:59 +0200

    Implement Create Purchase Handler

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { CreatePurchaseDto } from './dto/create-purchase.dto';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { User } from 'src/user/entities/user.entity';
 import { PurchaseService } from './purchase.service';
+import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Purchase } from './entities/purchase.entity';
 
+@ApiTags('Purchase')
 @Controller('purchase')
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
+
+  @ApiCreatedResponse({type: Purchase})
+  @ApiOperation({summary: 'Creates a new purchase'})
+  @ApiBearerAuth()
+  @Post()
+  async create(@CurrentUser() userId: User['id'], @Body() createPurchase: CreatePurchaseDto): Promise<Purchase> {
+      return this.purchaseService.create(userId, createPurchase)
+  }
 }
