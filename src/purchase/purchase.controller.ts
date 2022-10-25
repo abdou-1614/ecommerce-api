c30e3ae1d84cca39f89b1fdfb8e6b781261c616f
Abdou <kirakira1614@gmail.com>
2022-10-26 00:44:30 +0200

    Add isAdmin Decorator

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,10 +1,12 @@
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { User } from 'src/user/entities/user.entity';
 import { PurchaseService } from './purchase.service';
-import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Purchase } from './entities/purchase.entity';
+import { FindPurchaseDto } from './dto/find-purchase.dto';
+import { isAdmin } from 'src/common/decorators/isAdmin.decorator';
 
 @ApiTags('Purchase')
 @Controller('purchase')
@@ -17,5 +19,13 @@ export class PurchaseController {
   @Post()
   async create(@CurrentUser() userId: User['id'], @Body() createPurchase: CreatePurchaseDto): Promise<Purchase> {
       return this.purchaseService.create(userId, createPurchase)
+  }
+
+  @ApiOkResponse({type: [Purchase]})
+  @ApiOperation({summary: 'Admin Get Purchases'})
+  @isAdmin()
+  @Get('/admin')
+  async find(@Query() find: FindPurchaseDto): Promise<Purchase[]> {
+    return this.purchaseService.findPurchase(find)
   }
 }
