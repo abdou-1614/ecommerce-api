310f5c9f70f2530d88b1d6a2034876e38144d881
Abdou <kirakira1614@gmail.com>
2022-10-26 21:32:00 +0200

    Review Purchases Prodcuts

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,5 +1,6 @@
+import { ReviewDto } from './dto/review-purchase.dto';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, Req } from '@nestjs/common';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { User } from 'src/user/entities/user.entity';
 import { PurchaseService } from './purchase.service';
@@ -7,6 +8,7 @@ import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags
 import { Purchase } from './entities/purchase.entity';
 import { FindPurchaseDto } from './dto/find-purchase.dto';
 import { isAdmin } from 'src/common/decorators/isAdmin.decorator';
+import { Request } from 'express';
 
 @ApiTags('Purchase')
 @Controller('purchase')
@@ -27,5 +29,35 @@ export class PurchaseController {
   @Get('/admin')
   async find(@Query() find: FindPurchaseDto): Promise<Purchase[]> {
     return this.purchaseService.findPurchase(find)
+  }
+
+  @ApiOkResponse({type: [Purchase]})
+  @ApiOperation({summary: 'User Can Get All Her Purchases'})
+  @ApiBearerAuth()
+  @Get()
+  async findAllMine(@CurrentUser() userId: User['id'], @Query() findPurchase: FindPurchaseDto): Promise<Purchase[]> {
+    findPurchase.userId = userId
+    return this.purchaseService.findPurchase(findPurchase)
+  }
+
+  @ApiOkResponse({type: Purchase})
+  @ApiOperation({summary: 'Returns purchase by ID'})
+  @ApiBearerAuth()
+  @Get(':id')
+  async findOne(@Req() request: Request, @Param('id') purchaseId: string): Promise<Purchase> {
+    const {userId, userRole} = request.user as {
+      userId: string,
+      userRole: string
+    }
+
+    return this.purchaseService.findOne(purchaseId, userId, userRole)
+  }
+
+  @ApiOkResponse({type: Purchase})
+  @ApiOperation({summary: 'Review Product'})
+  @ApiBearerAuth()
+  @Patch('/review/:id')
+  async review(@CurrentUser() userId: User['id'], @Param('id') purchaseId: string, @Body() reviewDto: ReviewDto): Promise<Purchase> {
+      return this.purchaseService.review(purchaseId, userId, reviewDto)
   }
 }
