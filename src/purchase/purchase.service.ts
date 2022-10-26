cc2081fe4e4369544fe36615c51d58fe450654ba
Abdou <kirakira1614@gmail.com>
2022-10-26 21:33:36 +0200

    Implement Review Purchase Prducts

--- src/purchase/purchase.service.ts
+++ src/purchase/purchase.service.ts
@@ -1,9 +1,12 @@
+import { Role } from '@prisma/client';
 import { FindPurchaseDto } from './dto/find-purchase.dto';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
 import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import currency from 'currency.js';
 import { Purchase } from './entities/purchase.entity';
+import { NotOwnerPurchase } from './exceptions/purchase-owner.exception';
+import { ReviewDto } from './dto/review-purchase.dto';
 
 @Injectable()
 export class PurchaseService {
@@ -44,6 +47,45 @@ export class PurchaseService {
         })
 
         return purchases
+    }
+
+    async findOne(purchaseId: string, userId: string, userRole: string): Promise<Purchase> {
+        const purchase = await this.prisma.purchase.findUnique({
+            where: {id: purchaseId},
+            include: {
+                user: {select: {name: true}},
+                product: {select: {name: true}}
+            },
+            rejectOnNotFound: true
+        })
+
+        if(userRole !== Role.ADMIN && userId !== purchase.userId) {
+            throw new NotOwnerPurchase()
+        }
+
+        return purchase
+    }
+
+    async review(purchaseId: string, userId: string, reviewDto: ReviewDto):Promise<Purchase> {
+        const purchase = await this.prisma.purchase.findUnique({
+            where: {id: purchaseId},
+            rejectOnNotFound: true
+        })
+
+        if(userId !== purchase.userId) {
+            throw new NotOwnerPurchase()
+        }
+
+        return this.prisma.purchase.update({
+            where : {id: purchaseId},
+            data: {
+                ...reviewDto,
+            },
+            include: {
+                user: {select: {name: true}},
+                product: {select: {name: true}}
+            }
+        })
     }
 
 
